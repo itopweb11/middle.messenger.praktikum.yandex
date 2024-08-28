1b456fd3414b7405a055133e6e0330efa0aef283
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2024-08-28 17:39:50 +0300

    test error12

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 export {default as LoginPage} from './login';
-export {default as  PageRegistration} from './registration';
-export {default as PageChat} from './chat';
-export {default as  PageProfile} from './profile';
+export {default as  PageRegistration} from './registration/index.ts';
+export {default as PageChat} from './chat/index.ts';
+export {default as  PageProfile} from './profile/index.ts';
 export {default as Page404} from './404';
 export {default as Page500} from './500';
