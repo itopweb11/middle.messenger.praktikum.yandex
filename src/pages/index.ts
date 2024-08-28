3fa45d38d2ebc46ee19060340ceea224d15abc8b
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2024-08-28 17:41:47 +0300

    test error11

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 export {default as LoginPage} from './login';
-export {default as  PageRegistration} from './registration/index.ts';
-export {default as PageChat} from './chat/index.ts';
-export {default as  PageProfile} from './profile/index.ts';
+export {PageRegistration} from './registration/Registration.ts';
+export {PageChat} from './chat/Chat.ts';
+export {PageProfile} from './profile/Profile.ts';
 export {default as Page404} from './404';
 export {default as Page500} from './500';
