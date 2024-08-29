ca0731c516626a4fa5a1e8baa4310d9596885be6
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2024-08-29 11:25:42 +0300

    test error65

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 export {default as LoginPage} from './login';
-export {default as PageRegistration} from './registration';
-export {default as PageChat} from './chat';
-export {default as PageProfile} from './profile';
+export {PageRegistration} from './registration/registration';
+export {PageChat} from './chat/chat';
+export {PageProfile} from './profile/profile';
 export {default as Page404} from './404';
 export {default as Page500} from './500';
