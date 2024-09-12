f34397ec77bda71f7140fa64b49e921295855389
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2024-09-12 12:12:39 +0300

    test error!

--- mochaSetup.js
+++ mochaSetup.js
@@ -6,10 +6,14 @@ const jsdom = new JSDOM(`<body>
 <div id="app"></div>
 </body>`);
 
+// eslint-disable-next-line no-undef
 global.window = jsdom.window;
+// eslint-disable-next-line no-undef
 global.document = jsdom.window.document;
 
 // mocha
+// eslint-disable-next-line no-undef
 global.describe = describe;
+// eslint-disable-next-line no-undef
 global.it = it;
 
